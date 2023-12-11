20a72cfd7d28151883827322dd2045e7a1f248f8
Daniel Álvarez <danielalvarez.litscl.melchiot@protonmail.com>
2023-12-11 16:58:53 -0300

    Ok

--- src/cl/litscl/servletpaginacion/model/Persona.java
+++ src/cl/litscl/servletpaginacion/model/Persona.java
@@ -8,18 +8,23 @@ public class Persona {
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public int getEdad() {
 		return edad;
 	}
+	
 	public void setEdad(int edad) {
 		this.edad = edad;
 	}
